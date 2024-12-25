6ab070aa68463a44e3dbe161a2f56246e4a10de6
vuollyseg2 <follytech26@gmail.com>
2024-12-25 20:31:21 +0100

    update

---
+++ script.js
@@ -0,0 +1,6 @@
+// Example of JavaScript functionality
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Add event listeners or any JS functionality here
+  console.log('Bluclap website is ready!');
+});
